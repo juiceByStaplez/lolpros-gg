bc5a91ffb5373c06e20962bcfbf8f42be0026823
NicolasT <nicolastem@hotmail.fr>
2019-09-04 23:34:56 +0200

    remove models

--- src/Controller/LeagueOfLegends/Search/SearchController.php
+++ src/Controller/LeagueOfLegends/Search/SearchController.php
@@ -4,9 +4,7 @@ namespace App\Controller\LeagueOfLegends\Search;
 
 use App\Controller\APIController;
 use App\Manager\LeagueOfLegends\Search\SearchManager;
-use App\Model\LeagueOfLegends\Ladder\SearchResult;
 use FOS\RestBundle\Controller\Annotations\Get;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,20 +13,6 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class SearchController extends APIController
 {
-    /**
-     * @Get(path="")
-     */
-    public function getSearchAction(Request $request, SearchManager $searchManager): Response
-    {
-        $query = $request->get('q', null);
-
-        $results = new SearchResult();
-        $results->players = $searchManager->getSearchPlayers($query);
-        $results->accounts = $searchManager->getSearchRiotAccounts($query);
-
-        return $this->serialize($results, 'league.get_search');
-    }
-
     /**
      * @Get(path="/players/{name}")
      */
